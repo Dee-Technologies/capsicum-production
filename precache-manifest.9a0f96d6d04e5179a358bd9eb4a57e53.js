7e92d95917537668ce633ca8ba8c3fa5da4ac957
Dillon de Silva <dillon.desilva@geeksdiary.com>
2021-08-08 20:56:17 +1000

    first commit

---
+++ precache-manifest.9a0f96d6d04e5179a358bd9eb4a57e53.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88b779cc9fba8c25ca8318621b07cf4",
+    "url": "/capsicum/index.html"
+  },
+  {
+    "revision": "690c751f3b923fb66fbf",
+    "url": "/capsicum/static/css/main.1e3d489f.chunk.css"
+  },
+  {
+    "revision": "c3cd9f2ebe0a767090d3",
+    "url": "/capsicum/static/js/2.c7fc2d0a.chunk.js"
+  },
+  {
+    "revision": "31eb553c29004e5952d93408c74aa85e",
+    "url": "/capsicum/static/js/2.c7fc2d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690c751f3b923fb66fbf",
+    "url": "/capsicum/static/js/main.e65d62e7.chunk.js"
+  },
+  {
+    "revision": "2aea9cc103c7faf9a962801bb068404c",
+    "url": "/capsicum/static/js/main.e65d62e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7f85942579ac460733",
+    "url": "/capsicum/static/js/runtime-main.d66d8309.js"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/capsicum/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/capsicum/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "65c1e0086edb7a3b4b8ce7c2aca90729",
+    "url": "/capsicum/static/media/bg-banner.65c1e008.png"
+  },
+  {
+    "revision": "395cdf98dda19f60b49e93ffc35bfe2c",
+    "url": "/capsicum/static/media/capsicum-logo.395cdf98.svg"
+  },
+  {
+    "revision": "26747c77e2c324453b5b89cc7b5934e4",
+    "url": "/capsicum/static/media/capsicum-text-logo.26747c77.svg"
+  },
+  {
+    "revision": "54b282dbfd9bce128a20555df580870c",
+    "url": "/capsicum/static/media/capsicumlogo.54b282db.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/capsicum/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/capsicum/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/capsicum/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/capsicum/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/capsicum/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/capsicum/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/capsicum/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/capsicum/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ba449a0697388668ef05e043ffdeafca",
+    "url": "/capsicum/static/media/img-2-1200x1000.ba449a06.jpg"
+  },
+  {
+    "revision": "0ec264785c9b6efbed2651d84105f436",
+    "url": "/capsicum/static/media/landing.0ec26478.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/capsicum/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/capsicum/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/capsicum/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/capsicum/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/capsicum/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "278a0d250cc46de3a4f65a0791479e71",
+    "url": "/capsicum/static/media/profile.278a0d25.jpg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/capsicum/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/capsicum/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/capsicum/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/capsicum/static/media/team-4-800x800.23007132.jpg"
+  },
+  {
+    "revision": "7b19528b748cfc9da375f85875af352c",
+    "url": "/capsicum/static/media/testphotoone.7b19528b.JPG"
+  },
+  {
+    "revision": "f51c6b8664219e5da3ed89a45fb9a8fd",
+    "url": "/capsicum/static/media/testphototwo.f51c6b86.JPG"
+  }
+]);
