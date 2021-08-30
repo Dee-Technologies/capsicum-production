1c89a8b3bdbc686aa8c507ecf2d7fe61937fa86f
dillondesilva <dillon.desilva@geeksdiary.com>
2021-08-30 16:28:03 +1000

    site rebuild

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capsicum-production/precache-manifest.4ce00d77eb78652855182792047e0012.js"
+  "/capsicum-production/precache-manifest.752e3697246b34194e2f8da6a73b9cec.js"
 );
 
 self.addEventListener('message', (event) => {
